d4ebfded8268dd32007985e3755505b54371b7ce
Duwang <45719439+WhatABeatifulDuwang@users.noreply.github.com>
2024-05-24 10:51:42 +0200

    DoD and routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
