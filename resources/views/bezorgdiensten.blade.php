c202bcff53bc4d971bf62b0ae3f90da6f9359b0b
QB <QBsmartguy@gmail.com>
2024-06-05 15:16:04 +0200

    Page migration start

--- resources/views/bezorgdiensten.blade.php
+++ resources/views/bezorgdiensten.blade.php
@@ -1,3 +1,20 @@
 <x-layout title="Bezorgdiensten">
-
+    <div class="container">
+        <h1 class="textbezorg">Bezorgdiensten</h1>
+        <p class="textbezorg"> Als gebruiker wil ik informatie zien over bezorgdiensten zoals UPS, DHL, Homerr, zodat ik kan kiezen voor ophalen en verzenden.</p>
+        <p class="textbezorg">Kies een bezorgdienst:</p>
+        <ul>
+            <li><button class="bezorgdiensten" onclick="selectBezorgdienst('UPS')">UPS</button></li>
+            <li><button class="bezorgdiensten" onclick="selectBezorgdienst('DHL')">DHL</button></li>
+            <li><button class="bezorgdiensten" onclick="selectBezorgdienst('Homerr')">Homerr</button></li>
+    
+        </ul>
+        <div id="result"></div>
+    </div>
+    <script>
+        function selectBezorgdienst(bezorgdienst) {
+            document.getElementById('result').innerText = `Je hebt ${bezorgdienst} gekozen.`;
+    
+        }
+    </script>
 </x-layout>
