13b928e6935a73420a777f409b70ac4673234b91
Ody <odychen@gmail.com>
2024-06-14 14:22:11 +0200

    Auto stash before merge of "main" and "origin/main"

---
+++ database/migrations/2024_06_14_121000_create_requests_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('device_name');
+            $table->string('device_type');
+            $table->text('description');
+            $table->timestamps();
+            $table->foreign('account_id')->references('id')->on('accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('request');
+    }
+};
