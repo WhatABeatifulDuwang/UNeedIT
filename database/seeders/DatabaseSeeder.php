e0a36e9e10eb202d5fafcc78c7633c4beb2d6e33
Ody <odychen@gmail.com>
2024-06-17 10:12:29 +0200

    seeders works except request

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,23 +2,20 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Account;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class AccountSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Account::factory()
+            ->count(10)
+            ->create();
     }
 }
