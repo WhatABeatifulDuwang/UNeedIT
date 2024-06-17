887d1eca45a0ef541ab8563fd22c26e8ecdeaa4a
QB <QBsmartguy@gmail.com>
2024-06-17 10:36:56 +0200

    Small page update and div to section

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,10 +7,10 @@
 </head>
 <body>
     <nav id="navbar">
-        <div id="logoNav">
+        <section id="logoNav">
             <a href="{{ url('/')}}"><img src={{asset('images/cropped-logo%20UNEED-IT.png')}} alt="Logo"></a>
-        </div>
-        <div id="logoOptions">
+        </section>
+        <section id="logoOptions">
             <ul>
                 <li class="redc"><a href="{{ url('/') }}">Home</a></li>
                 <li class="bluec"><a href="{{ url('/overOns') }}">Over ons</a></li>
@@ -20,7 +20,7 @@
                 <li class="bluec"><a href="{{ url('/contact') }}">Contact</a></li>
                 <li class="redc"><a href="{{ url('/account') }}">Account</a></li>
             </ul>
-        </div>
+        </section>
     </nav>
     
     <main>
@@ -28,12 +28,12 @@
     </main>
 
     <footer id="footer">
-        <div id="address">
+        <section id="address">
             <img src="{{ asset('images/location.png') }}" alt="Location Icon">
             <p>Zuidbaan 514, 2841MD</p>
             <p>Moordrecht</p>
-        </div>
-        <div id="phoneNumber">
+        </section>
+        <section id="phoneNumber">
             <img src="{{ asset('images/phone.png') }}" alt="Phone Icon">
             <p>
                 Servicenummer:
@@ -44,13 +44,13 @@
                 <a href="tel:+31182820218">+3118-28-202-18</a>
             </p>
             <p>Bereikbaar van 09:00-18:00</p>
-        </div>
-        <div id="timeLabel">
+        </section>
+        <section id="timeLabel">
             <img src="{{ asset('images/clock.png') }}" alt="Clock Icon">
             <p>Ma t/m Vrij, 09:00 - 23:00</p>
             <p>Telefonisch bereikbaar</p>
             <p>voor abbonomenthouders</p>
-        </div>
+        </section>
     </footer>
 </body>
 </html>
