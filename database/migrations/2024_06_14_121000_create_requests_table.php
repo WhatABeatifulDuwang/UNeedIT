54d79bfe97e922169cad128b502a08b3c35e2db9
Ody <odychen@gmail.com>
2024-06-17 08:56:49 +0200

    Update migrations

--- database/migrations/2024_06_14_121000_create_requests_table.php
+++ database/migrations/2024_06_14_121000_create_requests_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Account;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,7 +20,7 @@ return new class extends Migration
             $table->string('device_type');
             $table->text('description');
             $table->timestamps();
-            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->foreignId('account_id')->references('id')->on('accounts');
         });
     }
 
@@ -30,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('request');
+        Schema::dropIfExists('requests');
     }
 };
