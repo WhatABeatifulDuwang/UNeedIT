37fd40775b3b5039e0ff4366c8160aefacabe6ee
QB <QBsmartguy@gmail.com>
2024-09-13 13:37:51 +0200

    Woopsie

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -12,7 +12,7 @@
             <section class="iframe">
                 <!-- Google Maps iframe voor locatie -->
                 <section class="location">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18..." width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d153.46767860491508!2d4.658284003608181!3d52.01630069829897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5d17dc64b3607%3A0xc5236f0d48a8cb14!2sZuidbaan%20514%2C%202841%20MJ%20Moordrecht!5e0!3m2!1snl!2snl!4v1726213290974!5m2!1snl!2snl" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </section>
             </section>
         </section>
