971d3ea6e713e8e10fb37c3f1e4a49930cc0727f
QB <QBsmartguy@gmail.com>
2024-09-18 11:36:24 +0200

    Made the calendar code significantly smaller

--- resources/views/afspraken.blade.php
+++ resources/views/afspraken.blade.php
@@ -2,7 +2,12 @@
 <x-layout title="Afspraken">
   <section class="afspraken-container">
     <div class="calendar">
-        <div class="month"><a href="#" class="nav"><i class="fas fa-angle-left"></i></a><div>January <span class="year">2019</span></div><a href="#" class="nav"><i class="fas fa-angle-right"></i></a></div>
+      <?php
+      $month = "September";
+      $year = 2024;
+      $days = 31;
+      ?>
+        <div class="month"><a href="#" class="nav"><i class="fas fa-angle-left"></i></a><div><?php echo "$month" ?> <span class="year"><?php echo "$year" ?></span></div><a href="#" class="nav"><i class="fas fa-angle-right"></i></a></div>
         <div class="days">
           <span>Mon</span>
           <span>Tue</span>
@@ -13,99 +18,12 @@
           <span>Sun</span>
         </div>
         <div class="dates">
-            <button>
-              <time>1</time>
-            </button>
-            <button>
-              <time>2</time>
-            </button>
-            <button>
-              <time>3</time>
-            </button>
-            <button>
-              <time>4</time>
-            </button>
-            <button>
-              <time>5</time>
-            </button>
-            <button>
-              <time>6</time>
-            </button>
-            <button>
-              <time>7</time>
-            </button>
-            <button>
-              <time>8</time>
-            </button>
-            <button>
-              <time>9</time>
-            </button>
-            <button>
-              <time>10</time>
-            </button>
-            <button>
-              <time>11</time>
-            </button>
-            <button>
-              <time>12</time>
-            </button>
-            <button>
-              <time>13</time>
-            </button>
-            <button>
-              <time>14</time>
-            </button>
-            <button>
-              <time>15</time>
-            </button>
-            <button>
-              <time>16</time>
-            </button>
-            <button>
-              <time>17</time>
-            </button>
-            <button class="today">
-              <time>18</time>
-            </button>
-            <button>
-              <time>19</time>
-            </button>
-            <button>
-              <time>20</time>
-            </button>
-            <button>
-              <time>21</time>
-            </button>
-            <button>
-              <time>22</time>
-            </button>
-            <button>
-              <time>23</time>
-            </button>
-            <button>
-              <time>24</time>
-            </button>
-            <button>
-              <time>25</time>
-            </button>
-            <button>
-              <time>26</time>
-            </button>
-            <button>
-              <time>27</time>
-            </button>
-            <button>
-              <time>28</time>
-            </button>
-            <button>
-              <time>29</time>
-            </button>
-            <button>
-              <time>30</time>
-            </button>
-            <button>
-              <time>31</time>
-            </button>
+          <!-- for loop om de datums aan te maken -->
+          <?php
+          for ($d = 1; $d <= $days; $d++) {
+            echo "<button><time>$d</time></button>";
+          }
+          ?>
         </div>
       </div>
   </section>
