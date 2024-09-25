144252fa0129a87223f55afa67fa59a32e156dc6
yafangchow <yafangchow@gmail.com>
2024-09-25 11:13:21 +0200

    Css aanmaken bij Account

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -24,16 +24,22 @@
             ?>
         </section>
         <section class="form">
+
+            <h1><span class="red-text">Uneed-</span><span class="blue-text">IT</span>  <span class="white-text">Account</span></h1>
+
             <form method="post">
                 <input type="email" id="email" name="email" placeholder="Enter your email">
                 <br>
                 <input type="password" id="password" name="password" placeholder="Enter your password">
                 <br>
+                <br>
                 <input id="postButton" type="submit" value="Login" name="Submit">
             </form>
+            <br>
             <a href="forgot-password.php">I forgot my password</a>
             <section class="bottom">
                 <p class="sign-up"> Don't have an account?<br>
+                    <br>
                     <a href="{{url('/accounts/create')}}">Sign Up</a></p>
             </section>
             <section id="confirmationMessage"></section>
