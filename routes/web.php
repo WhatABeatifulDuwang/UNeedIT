efca7946c39b086b337009ad92d0f67083d6af06
Ody <odychen@gmail.com>
2024-09-25 11:12:45 +0200

    web routes changed and fixed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\RequestController;
 use App\Models\Account;
 use Illuminate\Support\Facades\Route;
 
@@ -27,48 +29,12 @@ Route::get('/bezorgdiensten', function () {
     return view('bezorgdiensten');
 });
 
-Route::get('/accounts', function () {
-    return view('account');
-});
+Route::resources([
+    'accounts' => AccountController::class,
+    'requests' => RequestController::class
+]);
 
-Route::get('/accounts/create', function (){
-    return view('accounts.create');
-});
-
-Route::post('/accounts', function (\Illuminate\Http\Request $request) {
-    $account = $request->validate([
-        'name' => 'required|string|min:5|max:20',
-        'email' => 'required|unique:accounts,email|email:rfc,dns',
-        'password' => 'required|regex:/^.+@.+$/i|min:10|max:32',
-        'confirm-password' => 'required|regex:/^.+@.+$/i|min:10|max:32|same:password',
-    ]);
-
-    Account::create($account);
-    session_start();
-
-    return redirect('home.blade.php');
-});
-
-Route::get('/accounts/{account}', function (Account $account){
-    return view('accounts.show', compact('account'));
-});
-
-Route::get('/accounts/{account}/edit', function (Account $account){
-    return view('accounts.edit', compact('account'));
-});
-
-Route::put('/accounts/{account}', function (\Illuminate\Http\Request $request, Account $account){
-    $account->update($request->validate([
-        'name' => 'required|string|min:5|max:20',
-        'email' => 'required|unique:accounts,email|email:rfc,dns',
-        'password' => 'required|regex:/^.+@.+$/i|min:10|max:32',
-        'confirm-password' => 'required|regex:/^.+@.+$/i|min:10|max:32|same:password',
-    ]));
-
-    $account->save();
-
-    return redirect('/accounts');
-});
+Route::post('accounts/authenticate', [AccountController::class, 'authenticate'])->withoutMiddleware('auth');
 
 Route::get('/afspraken', function () {
     return view(view: 'afspraken');
