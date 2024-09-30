9c6163fd518f85c47bb0e881c58d153be8f5b835
CharmineX <boedhramkenneth@gmail.com>
2024-09-30 13:40:12 +0200

    Home page preparations
    beter is alles goed Ody

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,40 +1,34 @@
-<x-layout title="Home">
-    <section class="main-content main-page">
+<x-layout title="Home" id="Uneed-bg">
+    <section class="main-content">
         <section class="homeText">
             <!-- Logo van UNEED-IT -->
-            <img src="{{ asset('images/cropped-logo UNEED-IT(notext).png') }}" alt="UNEED-IT Logo" id="container">
+                <img src="{{ asset('images/cropped-logo UNEED-IT(notext).png') }}" alt="UNEED-IT Logo">
             <!-- Welkomsttekst met opmaak -->
             <p><span class="white-text">Voor al uw reparaties kunt u terecht bij </span>
                <span class="red-text">Uneed-</span><span class="blue-text">IT</span></p>
         </section>
     </section>
-    <section>
-        <h1>Iets komt hier beneden</h1>
-        <h1>Iets komt hier beneden</h1>
-    </section>
-
-{{--    <div class="container" id="container">--}}
-{{--        <div class="square square-1"></div>--}}
-{{--        <div class="square square-2"></div>--}}
-{{--    </div>--}}
+    <div class="secondary-content">
+        <section class="Uneed-Title">
+            <h1>Iets komt hier beneden</h1>
+            <h1>Iets komt hier beneden</h1>
+        </section></div>
 
 
-{{--    <script>--}}
-{{--        let container = document.getElementById('container');--}}
-{{--        let windowHeight = window.innerHeight;--}}
-{{--        let windowWidth = window.innerWidth;--}}
-{{--        let scrollArea = 1000 - windowHeight;--}}
-{{--        let square1 = document.getElementsByClassName('square')[0];--}}
-{{--        let square2 = document.getElementsByClassName('square')[1];--}}
+    <script>
+        let container = document.getElementById("Uneed-bg");
+        let windowHeight = window.innerHeight;
+        let windowWidth = window.innerWidth;
+        let scrollArea = 1000 - windowHeight;
+        let square1 = document.getElementsByClassName("homeText")[0];
 
-{{--        // update position of square 1 and square 2 when scroll event fires.--}}
-{{--        window.addEventListener('scroll', function() {--}}
-{{--            let scrollTop = window.pageYOffset || window.scrollTop;--}}
-{{--            let scrollPercent = scrollTop/scrollArea || 0;--}}
+        // Beweegt de locatie van de achtergrond als er gescrolled wordt.
+        window.addEventListener('scroll', function() {
+            let scrollTop = window.pageYOffset || window.scrollTop;
+            let scrollPercent = scrollTop/scrollArea || 0;
 
-{{--            square1.style.top = scrollPercent*window.innerHeight + 'px';--}}
-{{--            // square2.style.left = 800 - scrollPercent*window.innerWidth*0.6 + 'px';--}}
-{{--        });--}}
-{{--    </script>--}}
+            square1.style.bottom = scrollPercent*window.innerHeight + 'px';
+        });
+    </script>
 
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::get('/webshop', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
