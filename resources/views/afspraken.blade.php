ff36481142fd26d642e2ebea1212c6b239e12501
QB <QBsmartguy@gmail.com>
2024-10-25 08:53:24 +0200

    Jk Fixed it now

--- resources/views/afspraken.blade.php
+++ resources/views/afspraken.blade.php
@@ -21,7 +21,7 @@ $firstDayOfMonth = date('N', strtotime("$currentYear-$currentMonth-01"));
             @endif
             <section class="month">
                 <a href="?month=<?php echo $currentMonth - 1 <= 0 ? 12 : $currentMonth - 1; ?>&year=<?php echo $currentMonth - 1 <= 0 ? $currentYear - 1 : $currentYear; ?>" class="nav">
-                    <i class="fas fa-angle-left">
+                    <i class="fas fa-angle-left"><</i>
                 </a>
                 <section>
                     <?php echo date('F', strtotime("$currentYear-$currentMonth-01")); ?>
